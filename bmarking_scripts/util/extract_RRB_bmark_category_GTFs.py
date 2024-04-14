f03a60951342ffa21900b6a56e3e3c9d631417b0
Brian Haas <brianjohnhaas@gmail.com>
2024-04-13 20:28:08 -0400

    add audit summary file

--- bmarking_scripts/util/extract_RRB_bmark_category_GTFs.py
+++ bmarking_scripts/util/extract_RRB_bmark_category_GTFs.py
@@ -16,19 +16,35 @@ def main():
     parser = argparse.ArgumentParser(description="extract GTF records according to benchmarking categories",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--gtf", type=str, required=True, help="reco gtf file")
+    parser.add_argument("--reco_gtf", type=str, required=True, help="reco gtf file")
+    parser.add_argument("--truth_gtf", type=str, required=True, help="reco gtf file")
 
     parser.add_argument("--tracking", type=str, required=True, help="tracking file from gffcompare")
 
     args = parser.parse_args()
     
-
-    gtf_file = args.gtf
-    tracking_file = args.tracking
+    reco_gtf_file = args.reco_gtf
+    truth_gtf_file = args.truth_gtf
+    
+    input_tracking_file = args.tracking
     
 
-    transcript_to_gtf_txt = parse_transcripts_from_gtf(gtf_file)
+    reco_transcript_to_gtf_txt = parse_transcripts_from_gtf(reco_gtf_file)
+    
+    truth_transcript_to_gtf_txt = parse_transcripts_from_gtf(truth_gtf_file)
+    
+    base_gtf_name = os.path.basename(reco_gtf_file)
 
+    types = ('novel_fn', 'known_fn', 'known_tp', 'novel_tp', 'novel_fp')
+
+    fhs = dict()
+    for ctype in types:
+        fhs[ctype] = open(base_gtf_name + "." + ctype + ".gtf", "wt")
+
+
+    ofh_audit = open(base_gtf_name + ".refcompare-audit.txt", "wt")
+    
+        
     # code chunk adapted from isoquant bmarking below:
     for line in open(input_tracking_file, "r"):
         # column[0]: unique internal id for the transfrag
@@ -38,23 +54,54 @@ def main():
         # columns[4:]: each following column showns the transcript for each sample/tool
         transcript_columns = line.strip().split()
 
+        
+        reco_transcript_id = None
+        if transcript_columns[6] != '-':
+            reco_transcript_id = transcript_columns[6].split("|")[1]
+
+
+        ref_gene_id = None
+        ref_transcript_id = None
+        if transcript_columns[4] != '-':
+            ref_transcript_id = transcript_columns[4].split("|")[1]
+            ref_gene_id = transcript_columns[4].split("|")[0].split(":")[1]
+            
+        vals = [ref_gene_id, ref_transcript_id, reco_transcript_id]
+            
         if transcript_columns[4] != '-' and transcript_columns[5] == '-' and transcript_columns[6] == '-':
-            novel_fn += 1
+            #novel_fn += 1
+            print(truth_transcript_to_gtf_txt[ref_transcript_id], file=fhs['novel_fn'])
+            vals.insert(0, "novel_fn")
+            
         elif transcript_columns[4] != '-' and transcript_columns[5] != '-' and transcript_columns[6] == '-':
-            known_fn += 1
+            #known_fn += 1
+            print(truth_transcript_to_gtf_txt[ref_transcript_id], file=fhs['known_fn'])
+            vals.insert(0, "known_fn")
+            
         elif transcript_columns[4] != '-' and transcript_columns[5] != '-' and transcript_columns[6] != '-':
-            known_tp += 1
+            #known_tp += 1
+            print(reco_transcript_to_gtf_txt[reco_transcript_id], file=fhs['known_tp'])
+            vals.insert(0, "known_tp")
+                        
         elif transcript_columns[4] != '-' and transcript_columns[5] == '-' and transcript_columns[6] != '-':
-            novel_tp += 1
+            #novel_tp += 1
+            print(reco_transcript_to_gtf_txt[reco_transcript_id], file=fhs['novel_tp'])
+            vals.insert(0, "novel_tp")
+            
         elif transcript_columns[4] == '-' and transcript_columns[5] == '-' and transcript_columns[6] != '-':
-            novel_fp += 1
+            #novel_fp += 1
+            print(reco_transcript_to_gtf_txt[reco_transcript_id], file=fhs['novel_fp'])
+            vals.insert(0, "novel_fp")
+            
         else:
             print("WARNING: This should not have happened! Current line: " + str(transcript_columns))
 
 
 
+        print("\t".join([str(x) for x in vals]), file=ofh_audit)
 
 
+    sys.exit(0)
 
 
 def parse_transcripts_from_gtf(gtf_file):
